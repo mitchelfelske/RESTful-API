bcac89dc244db0359854310385422f48f2cff60c
mitchel.felske <mitchel.felske@ufsc.br>
2018-09-08 15:19:29 -0300

    Todos API: Added controller and test suite for getting a todo item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,24 @@
 class ItemsController < ApplicationController
+  before_action :set_todo
+  before_action :set_todo_item, only: [:show]
 
   # GET /todos/:todo_id/items
   def index
-    @todo = Todo.find(params[:todo_id])
     json_response(@todo.items)
   end
+
+  # GET /todos/:todo_id/items/:id
+  def show
+    json_response(@item)
+  end
+end
+
+private
+
+def set_todo
+  @todo = Todo.find(params[:todo_id])
+end
+
+def set_todo_item
+  @item = @todo.items.find_by!(id: params[:id])
 end
--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -5,8 +5,9 @@ RSpec.describe 'Items API', type: :request do
   let!(:todo) { create(:todo) }
   let!(:items) { create_list(:item, 20, todo_id: todo.id) }
   let(:todo_id) { todo.id }
+  let(:id) { items.first.id }
 
-  # Test suit for getting todo items
+  # Test suit for getting a todo items list
   describe 'GET /todos/:todo_id/items' do
     before { get "/todos/#{todo_id}/items" }
 
@@ -30,6 +31,33 @@ RSpec.describe 'Items API', type: :request do
 
       it 'returns a not found message' do
         expect(response.body).to match(/Couldn't find Todo with 'id'=0/)
+      end
+    end
+  end
+
+  # Test suit for getting a todo item
+  describe  'GET /todos/:todo_id/items/:id' do
+    before { get "/todos/#{todo_id}/items/#{id}" }
+
+    context 'when todo item exists' do
+      it 'returns request succeeded status code' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns the item' do
+        expect(json['id']).to eq(id)
+      end
+    end
+
+    context 'when todo item does not exist' do
+      let(:id) { 0 }
+
+      it 'returns not found status code' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Item/)
       end
     end
   end
