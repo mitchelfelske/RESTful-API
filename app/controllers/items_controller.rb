a0b8496504483b9bba8349aca5c59478b2e69b4b
mitchel.felske <mitchel.felske@ufsc.br>
2018-09-08 15:55:45 -0300

    Todos API: Added controller and test suite for creating a todo item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,14 +11,25 @@ class ItemsController < ApplicationController
   def show
     json_response(@item)
   end
-end
 
-private
+  # POST /todos/:todo_id/items
+  def create
+    @todo.items.create!(item_params)
+    json_response(@todo, :created)
+  end
 
-def set_todo
-  @todo = Todo.find(params[:todo_id])
-end
+  private
 
-def set_todo_item
-  @item = @todo.items.find_by!(id: params[:id])
+  def item_params
+    params.permit(:name, :done)
+  end
+
+  def set_todo
+    @todo = Todo.find(params[:todo_id])
+  end
+
+  def set_todo_item
+    @item = @todo.items.find_by!(id: params[:id])
+  end
+
 end
--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -61,4 +61,31 @@ RSpec.describe 'Items API', type: :request do
       end
     end
   end
+
+  # Test suit for creating an item
+  describe 'POST /todos/:todo_id/items' do
+    let(:valid_attributes) { { name: "Todo Item 1", done: true } }
+
+    context 'when request attributes are valid' do
+      before { post "/todos/#{todo_id}/items", params: valid_attributes}
+
+      it 'returns created status code' do
+        expect(response).to have_http_status(201)
+      end
+    end
+
+    context 'when an invalid request' do
+      context 'because all params are missing' do
+        before { post "/todos/#{todo_id}/items", params: { } }
+
+        it 'returns unprocessable entity status code' do
+          expect(response).to have_http_status(422)
+        end
+
+        it 'returns a validation failure message' do
+          expect(response.body).to match(/Validation failed: Name can't be blank/)
+        end
+      end
+    end
+  end
 end
